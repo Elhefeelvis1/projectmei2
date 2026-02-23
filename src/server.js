a8c113cfc9f6e7a50b49182f9736ab3279438f13
Mmaduabuchi <elvismarcelinus.ei@gmail.com>
2026-02-23 08:30:14 +0100

    move files to frontend folder

--- src/server.js
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-import session from 'express-session';
-import passport from 'passport';
-import { Strategy } from "passport-local";
-import env from "dotenv";
-import bcrypt from 'bcrypt';
-
